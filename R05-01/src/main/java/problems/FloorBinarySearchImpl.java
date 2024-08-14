390f80debc5a0564c1b01f2a1e7d3fa682804f58
Guilherme Jose <140714108+gjose05@users.noreply.github.com>
2024-08-14 01:55:26 -0300

    Add files via upload

--- R05-01/src/main/java/problems/FloorBinarySearchImpl.java
+++ R05-01/src/main/java/problems/FloorBinarySearchImpl.java
@@ -4,55 +4,76 @@ import util.Util;
 
 public class FloorBinarySearchImpl implements Floor {
 
-	@Override
-	public Integer floor(Integer[] array, Integer x) {
-		if (array == null || array.length == 0){
-			return null;
-		}
-		this.QuickSort(array, 0, array.length-1);
-		if (x <= array[0]){
-			return null;
-		}
-		if ( x>= array[array.length-1]){
-			return array[array.length-1];
-		}
-		return array[BuscaBinaria(array, 0, array.length-1, x)];
-	}
+    @Override
+    public Integer floor(Integer[] array, Integer x) {
+        if (array.length > 0){
+            quickSortMedianOfThree(array, 0, array.length-1);
+            if (x > array[0]){
+                int floorIndex = binarySearch(array, 0, array.length-1, x);
+                return array[floorIndex];
+            }
+        }
+        return null;
 
-	private Integer BuscaBinaria(Integer[] array,int left, int right, int x){
-		if (left==right){
-			return left-1;
-		}
-		int meio = (left+right)/2;
-		if (array[meio] == x || array[meio] == array[0]){
-			return meio;
-		}
-		else if (x>array[meio]){
-			return BuscaBinaria(array,meio+1,array.length-1,x);
-		}
-		else{
-			return BuscaBinaria(array, 0, meio,x);
-		}
-	}
-	private Integer[] QuickSort(Integer[] array, int left, int right){
-		if (right > left){
-			int pivot = partition(array,left,right);
-			QuickSort(array,left,pivot-1);
-			QuickSort(array,pivot+1,right);
-		}
-		return array;
-	}
-	private int partition(Integer[] array, int left, int right){
-		int pivot = left;
-		int contador = left;
-		for (int i = left+1; i <= right; i++){
-			if (array[pivot] > array[i]){
-				contador++;
-				Util.swap(array, contador, i);
-			}
-		}
-		Util.swap(array, pivot, contador);
-		return contador;
-	}
+    }
 
+    private void quickSortMedianOfThree(Integer[] array, int leftIndex, int rightIndex) {
+        if (leftIndex < rightIndex) {
+
+            findPivot(array, leftIndex, rightIndex);
+            Util.swap(array, rightIndex/2, rightIndex-1);
+
+            int pivotIndex = partition(array, leftIndex, rightIndex);
+
+            quickSortMedianOfThree(array, leftIndex, pivotIndex - 1);
+            quickSortMedianOfThree(array, pivotIndex + 1, rightIndex);
+        }
+    }
+
+    private int partition(Integer[] array, int leftIndex, int rightIndex) {
+        Integer pivot = array[leftIndex];
+        int countIndex = leftIndex;
+        
+        for (int j = countIndex + 1; j <= rightIndex; j++) {
+            if (array[j].compareTo(pivot) <= 0) {
+                countIndex++;
+                Util.swap(array, countIndex, j);
+            }
+        }
+
+        Util.swap(array, leftIndex, countIndex);
+        return countIndex;
+    }
+
+    private void findPivot(Integer[] array, int leftIndex, int rightIndex) {
+        int highest = leftIndex;
+        int secondHighest = rightIndex/2;
+
+        if(array[secondHighest].compareTo(array[highest]) >= 0){
+            highest = secondHighest;
+            secondHighest = leftIndex;
+        }
+        if(array[rightIndex].compareTo(array[highest]) >= 0){
+            secondHighest = highest;
+            highest = rightIndex;
+        }
+
+        Util.swap(array, rightIndex, highest);
+        Util.swap(array, rightIndex/2, secondHighest);
+    }
+
+    private int binarySearch(Integer[] array,int leftIndex, int rightIndex, int x){
+        if (leftIndex < rightIndex){    
+            int middle = (leftIndex + rightIndex)/2;
+            if (x == array[middle]) {
+                return middle;
+            }
+            else if (x > array[middle]){
+                return binarySearch(array, middle+1, rightIndex, x);
+            }
+            else if (x < array[middle])
+                return binarySearch(array, leftIndex, middle-1, x);
+        }
+        return 0;
+    }
 }
