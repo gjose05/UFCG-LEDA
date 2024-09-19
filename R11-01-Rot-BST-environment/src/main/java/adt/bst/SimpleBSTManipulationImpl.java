3f50caf601d091941a2276e9a79a193cd8e83718
Guilherme Jose <140714108+gjose05@users.noreply.github.com>
2024-09-19 15:20:41 -0300

    Update SimpleBSTManipulationImpl.java

--- R11-01-Rot-BST-environment/src/main/java/adt/bst/SimpleBSTManipulationImpl.java
+++ R11-01-Rot-BST-environment/src/main/java/adt/bst/SimpleBSTManipulationImpl.java
@@ -10,20 +10,57 @@ public class SimpleBSTManipulationImpl<T extends Comparable<T>> implements Simpl
 
 	@Override
 	public boolean equals(BST<T> tree1, BST<T> tree2) {
-		// TODO Implement this method
-		throw new UnsupportedOperationException("Not implemented yet!");
+		BSTNode<T> node1 = (BSTNode<T>) tree1.getRoot();
+		BSTNode<T> node2 = (BSTNode<T>) tree2.getRoot();
+		return equalsRecursivo(node1, node2);
+	}
+	private boolean equalsRecursivo(BSTNode<T> node1, BSTNode<T> node2){
+		if (node1.isEmpty() && node2.isEmpty()){
+			return true;
+		}
+		if (node1.getData().equals(node2.getData())){
+			return true;
+		}
+		if (!node1.getData().equals(node2.getData()) || node1.isEmpty() || node2.isEmpty()){
+			return false;
+		}
+		return (equalsRecursivo((BSTNode<T>) node1.getLeft(),(BSTNode<T>)node2.getLeft()) && equalsRecursivo((BSTNode<T>) node1.getRight(),(BSTNode<T>)node2.getRight()));
 	}
 
 	@Override
 	public boolean isSimilar(BST<T> tree1, BST<T> tree2) {
-		// TODO Implement this method
-		throw new UnsupportedOperationException("Not implemented yet!");
+		BSTNode<T> node1 = (BSTNode<T>) tree1.getRoot();
+		BSTNode<T> node2 = (BSTNode<T>) tree2.getRoot();
+		return isSimilarRecursivo(node1,node2);
+	}
+	private boolean isSimilarRecursivo(BSTNode<T> node1, BSTNode<T> node2){
+		if (node1.isEmpty() && node2.isEmpty()){
+			return true;
+		}
+		else if (node1.isEmpty()||node2.isEmpty()){
+			return false;
+		}
+		return (isSimilarRecursivo((BSTNode<T>)node1.getLeft(),(BSTNode<T>)node2.getLeft()) && isSimilarRecursivo((BSTNode<T>)node1.getRight(), (BSTNode<T>)node2.getRight()));
 	}
 
 	@Override
 	public T orderStatistic(BST<T> tree, int k) {
-		// TODO Implement this method
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if (k > tree.size()){
+			return null;
+		}
+		BSTNode<T> minimo = tree.minimum();
+		return orderStatisticsRecursive(tree,minimo,k);
+	}
+	private T orderStatisticsRecursive(BST<T> arvore, BSTNode<T> menor, int k){
+		if(k==0){
+			if (!menor.isEmpty()){
+				return menor.getData();
+			}
+		}
+		arvore.remove(menor.getData());
+		BSTNode<T> novoMenor = arvore.minimum();
+		orderStatisticsRecursive(arvore, novoMenor, k-1);
+		return null;
 	}
 
 }
