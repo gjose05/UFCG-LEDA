11f20d1e636eac55b9465654115bd18eca613325
Guilherme Jose <140714108+gjose05@users.noreply.github.com>
2024-09-19 15:19:20 -0300

    Update FloorCeilBSTImpl.java

--- R11-01-Rot-BST-environment/src/main/java/adt/bst/extended/FloorCeilBSTImpl.java
+++ R11-01-Rot-BST-environment/src/main/java/adt/bst/extended/FloorCeilBSTImpl.java
@@ -1,6 +1,7 @@
 package adt.bst.extended;
 
 import adt.bst.BSTImpl;
+import adt.bst.BSTNode;
 
 /**
  * Note que esta classe estende sua implementacao de BST (BSTImpl).
@@ -11,14 +12,57 @@ public class FloorCeilBSTImpl extends BSTImpl<Integer> implements FloorCeilBST {
 
 	@Override
 	public Integer floor(Integer[] array, double numero) {
-		//TODO Implemente seu codigo aqui
-		throw new UnsupportedOperationException("Not implemented yet!");
+		for (int i = 0; i < array.length; i++){
+			this.insert(array[i]);
+		}
+		return floorRecursivo((BSTNode<Integer>) this.root, numero);
+	}
+	private Integer floorRecursivo(BSTNode<Integer> node, double numero){
+		Integer resp = null;
+		if(!node.isEmpty()){
+			if (node.getData() == numero){
+				return node.getData();
+			}
+			if (node.getData() > numero){
+				return floorRecursivo((BSTNode<Integer>) node.getLeft(), numero);
+			}
+			else{
+				resp = floorRecursivo((BSTNode<Integer>) node.getRight(),numero);
+			}
+		}
+		if (resp != null){
+			return resp;
+		}
+		else{
+			return node.getData();
+		}
 	}
 
 	@Override
 	public Integer ceil(Integer[] array, double numero) {
-		//TODO Implemente seu codigo aqui
-		throw new UnsupportedOperationException("Not implemented yet!");
+		for (int i =0; i < array.length; i++){
+			this.insert(array[i]);
+		}
+		return ceilRecursivo((BSTNode<Integer>) this.root, numero);
 	}
-
+	private Integer ceilRecursivo(BSTNode<Integer> node, double numero){
+		Integer resp = null;
+		if(!node.isEmpty()){
+			if (node.getData() == numero){
+				return node.getData();
+			}
+			if (node.getData() < numero){
+				return ceilRecursivo((BSTNode<Integer>) node.getRight(), numero);
+			}
+			else{
+				resp = ceilRecursivo((BSTNode<Integer>) node.getLeft(), numero);
+			}
+		}
+		if (resp != null){
+			return resp;
+		}
+		else{
+			return node.getData();
+		}
+	}
 }
