5a2ca100438cdd3043006c78eeb06cd840ee2be4
Guilherme Jose <140714108+gjose05@users.noreply.github.com>
2024-09-20 11:29:22 -0300

    Add files via upload

---
+++ R12-01-Rot-HeapBinaria-environment/src/main/java/adt/heap/HeapImpl.java
@@ -0,0 +1,184 @@
+package adt.heap;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+
+import util.Util;
+
+/**
+ * O comportamento de qualquer heap é definido pelo heapify. Neste caso o
+ * heapify dessa heap deve comparar os elementos e colocar o menor sempre no
+ * topo. Ou seja, admitindo um comparador normal (responde corretamente 2 < 3),
+ * essa heap deixa os elementos menores no topo. Essa comparação não é feita
+ * diretamente com os elementos armazenados, mas sim usando um comparator. 
+ * Dessa forma, dependendo do comparator, a heap pode funcionar como uma max-heap 
+ * ou min-heap.
+ */
+public class HeapImpl<T extends Comparable<T>> implements Heap<T> {
+
+	protected T[] heap;
+	protected int index = -1;
+	/**
+	 * O comparador é utilizado para fazer as comparações da heap. O ideal é
+	 * mudar apenas o comparator e mandar reordenar a heap usando esse
+	 * comparator. Assim os metodos da heap não precisam saber se vai funcionar
+	 * como max-heap ou min-heap.
+	 */
+	protected Comparator<T> comparator;
+
+	private static final int INITIAL_SIZE = 20;
+	private static final int INCREASING_FACTOR = 10;
+
+	/**
+	 * Construtor da classe. Note que de inicio a heap funciona como uma
+	 * min-heap. OU seja, voce deve considerar que a heap usa o comparator
+	 * interno e se o comparator responde compare(x,y) < 0 entao o x eh menor
+	 * e sobe na heap.
+	 */
+	@SuppressWarnings("unchecked")
+	public HeapImpl(Comparator<T> comparator) {
+		this.heap = (T[]) (new Comparable[INITIAL_SIZE]);
+		this.comparator = comparator;
+	}
+
+	// /////////////////// METODOS IMPLEMENTADOS
+	private int parent(int i) {
+		return (i - 1) / 2;
+	}
+
+	/**
+	 * Deve retornar o indice que representa o filho a esquerda do elemento
+	 * indexado pela posicao i no vetor
+	 */
+	private int left(int i) {
+		return (i * 2 + 1);
+	}
+
+	/**
+	 * Deve retornar o indice que representa o filho a direita do elemento
+	 * indexado pela posicao i no vetor
+	 */
+	private int right(int i) {
+		return (i * 2 + 1) + 1;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return (index == -1);
+	}
+
+	@Override
+	public T[] toArray() {
+		ArrayList<T> resp = new ArrayList<T>();
+		for (int i = 0; i <= this.index; i++) {
+			resp.add(this.heap[i]);
+		}
+		return (T[])resp.toArray(new Comparable[0]);
+	}
+
+	// ///////////// METODOS A IMPLEMENTAR
+	/**
+	 * Valida o invariante de uma heap a partir de determinada posicao, que pode
+	 * ser a raiz da heap ou de uma sub-heap. O heapify deve usar o comparator
+	 * para subir os elementos na heap.
+	 */
+	private void heapify(int position) {
+		int leftPosition = left(position);
+		int rightPosition = right(position);
+		int auxPosicao = position;
+		if (this.comparator.compare(this.heap[auxPosicao], this.heap[leftPosition]) < 0){
+			auxPosicao = leftPosition;
+		}
+		if (this.comparator.compare(this.heap[auxPosicao], this.heap[rightPosition]) < 0){
+			auxPosicao = rightPosition;
+		}
+		if (auxPosicao != position){
+			Util.swap(heap, auxPosicao, position);
+			heapify(auxPosicao);
+		}
+	}
+
+	@Override
+	public void insert(T element) {
+		// ESSE CODIGO E PARA A HEAP CRESCER SE FOR PRECISO. NAO MODIFIQUE
+		if (index == heap.length - 1) {
+			heap = Arrays.copyOf(heap, heap.length + INCREASING_FACTOR);
+		}
+		// /////////////////////////////////////////////////////////////////
+		index++;
+		this.heap[index] = element;
+		int auxPosicao = index;
+		while (auxPosicao > 0 && this.comparator.compare(this.heap[parent(auxPosicao)], this.heap[auxPosicao]) < 0){
+			Util.swap(this.heap, auxPosicao, parent(auxPosicao));
+			auxPosicao = parent(auxPosicao);
+		}
+	}
+
+	@Override
+	public void buildHeap(T[] array) {
+		this.heap = array;
+		this.index = array.length-1;
+		for (int i = 0; i < index ; i++){
+			heapify(i);
+		}
+	}
+
+	@Override
+	public T extractRootElement() {
+		if (!isEmpty()){
+			T resp = this.heap[0];
+			Util.swap(this.heap, 0, index);
+			heapify(0);
+			return resp;
+		}
+		return null;
+	}
+
+	@Override
+	public T rootElement() {
+		T resp = null;
+		if (this.heap[0] != null){
+			resp = this.heap[0];
+		}
+		return resp;
+	}
+
+	@Override
+	public T[] heapsort(T[] array) {
+		T[] arrayResp = (T[]) new Comparator[size()];
+		buildHeap(array);
+		if (this.comparator.compare(array[0], array[index])>0){
+			for (int i = index; i >= 0; i--){
+				arrayResp[i] = extractRootElement();
+			}
+		}
+		else{
+			for (int i = 0; i < array.length; i++){
+				arrayResp[i] = extractRootElement();
+			}
+		}
+		return arrayResp;
+	}
+
+	@Override
+	public int size() {
+		if(index != -1){
+			return index+1;
+		}
+		return 0;
+	}
+
+	public Comparator<T> getComparator() {
+		return comparator;
+	}
+
+	public void setComparator(Comparator<T> comparator) {
+		this.comparator = comparator;
+	}
+
+	public T[] getHeap() {
+		return heap;
+	}
+
+}
